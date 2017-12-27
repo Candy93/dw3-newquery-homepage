d1f4c5acc61dca7a742f8041a5fc17ef01610ba7
gp <guopeng@bonc.com.cn>
2017-12-27 15:37:41 +0800

    测试github

--- src/main/java/com/bonc/dw3/controller/HomepageController.java
+++ src/main/java/com/bonc/dw3/controller/HomepageController.java
@@ -27,7 +27,6 @@ public class HomepageController {
 
     /**
      * 1.头部栏组件接口
-     *
      * @Parameter paramMap 例：{"userId":"41","token":"2"}
      * @Author gp
      * @Date 2017/5/27
@@ -37,7 +36,6 @@ public class HomepageController {
     public String headerSelect(@ApiParam("请求参数json串") @RequestBody Map<String, Object> paramMap,
                                Model model) {
         Map<String, Object> resMap = homepageService.headerSelect();
-
         model.addAttribute("resMap", resMap);
         return "headerSelect";
     }
@@ -45,7 +43,6 @@ public class HomepageController {
 
     /**
      * 3.模块选项卡接口
-     *
      * @Parameter paramMap 例：{"userId":"41","token":"2","markType":"999"}
      * @Author gp
      * @Date 2017/5/27
@@ -56,7 +53,6 @@ public class HomepageController {
                             Model model) {
         String markType = paramMap.get("markType").toString();
         List<Map<String, String>> resList = homepageService.moduleTab(markType);
-
         model.addAttribute("resList", resList);
         return "moduleTab";
     }
@@ -64,7 +60,6 @@ public class HomepageController {
 
     /**
      * 6.搜索接口:全部搜索
-     *
      * @Parameter paramMap 例：{"userId":"41","token":"2","searchType":"999","search":"","tabId":"-1","numStart":"1","num":"10"}
      * @Author gp
      * @Date 2017/5/16
@@ -79,12 +74,10 @@ public class HomepageController {
         String tabId = paramMap.get("tabId").toString();
         String numStart = paramMap.get("numStart").toString();
         String num = paramMap.get("num").toString();
-
         //es查询参数处理
         String paramStr = userId + "," + searchType + "," + search + "," + tabId + "," + numStart + "," + num;
         //查询es并拼接结果
         Map<String, Object> resMap = homepageService.allSearch(paramStr, numStart, num, userId);
-
         model.addAttribute("resMap", resMap);
         return "allSearch";
     }
@@ -92,7 +85,6 @@ public class HomepageController {
 
     /**
      * 6-1.搜索：指标接口
-     *
      * @Parameter paramMap 例：{"userId":"41","token":"2","searchType":"1","search":"","dayOrmonth":"-1","numStart":"1","num":"10","area":"010","date":"2016-10-01"}
      * @Author gp
      * @Date 2017/5/31
@@ -109,7 +101,6 @@ public class HomepageController {
         String num = paramMap.get("num").toString();
         String area = paramMap.get("area").toString();
         String date = paramMap.get("date").toString();
-
         //es查询参数处理
         String paramStr = userId + "," + searchType + "," + search + "," + dayOrmonth + "," + numStart + "," + num;
         //查询es并拼接结果
@@ -121,14 +112,12 @@ public class HomepageController {
         } else {
             model.addAttribute("resMap", resMap);
         }
-
         return "indexSearch";
     }
 
 
     /**
      * 7.搜索：专题接口
-     *
      * @Parameter paramMap 例：{"userId":"41","token":"2","searchType":"2","search":"","tabId":"-1","numStart":"1","num":"10"}
      * @Author gp
      * @Date 2017/5/31
@@ -145,12 +134,10 @@ public class HomepageController {
         String tabId = paramMap.get("tabId").toString();
         String numStart = paramMap.get("numStart").toString();
         String num = paramMap.get("num").toString();
-
         //es查询参数处理
         String paramStr = userId + "," + searchType + "," + search + "," + tabId + "," + numStart + "," + num;
         //查询es获得数据
         resMap = homepageService.specialSearch(paramStr, numStart, num);
-
         model.addAttribute("resMap", resMap);
         return "specialSearch";
     }
@@ -158,7 +145,6 @@ public class HomepageController {
 
     /**
      * 8.搜索：报告接口
-     *
      * @Parameter paramMap 例：{"userId":"41","token":"2","searchType":"3","search":"","numStart":"1","num":"10"}
      * @Author gp
      * @Date 2017/6/9
@@ -174,18 +160,17 @@ public class HomepageController {
         String search = paramMap.get("search").toString();
         String numStart = paramMap.get("numStart").toString();
         String num = paramMap.get("num").toString();
-
         //tabId用-1占位
         String paramStr = userId + "," + searchType + "," + search + "," + "-1," + numStart + "," + num;
         //查询es获得数据
         resMap = homepageService.reportPPTSearch(paramStr, numStart, num);
-
         model.addAttribute("resMap", resMap);
         return "reportSearch";
     }
 
 
     /**
+     * 9.搜索：报表搜索接口
      * @Author gp
      * @Date 2017/11/22
      */
@@ -204,15 +189,13 @@ public class HomepageController {
         String paramStr = userId + "," + searchType + "," + search + "," + tabId + "," + numStart + "," + num;
         //查询es获得数据
         resultMap = homepageService.statementSearch(paramStr, numStart, num);
-
         model.addAttribute("resMap", resultMap);
         return "statementSearch";
     }
 
 
     /**
-     * 9.地域组件接口
-     *
+     * 10.地域组件接口
      * @Parameter paramMap 例：{"userId":"41","token":"2"}
      * @Author gp
      * @Date 2017/6/9
@@ -228,8 +211,7 @@ public class HomepageController {
 
 
     /**
-     * 10.日期组件接口
-     *
+     * 11.日期组件接口
      * @Parameter paramMap 例：{"userId":"41","token":"2","dateType":"1"}
      * @Author gp
      * @Date 2017/6/9
@@ -241,7 +223,6 @@ public class HomepageController {
         String dateType = paramMap.get("dateType").toString();
         String userId= paramMap.get("userId").toString();
         String date = homepageService.getMaxDate(dateType, userId);
-
         model.addAttribute("date", date);
         return "date";
     }
